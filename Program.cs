414c35e5c48dff401013991bb282ac9e8e95304c
Kate Sutin <ksutin@renaissancecapital.com>
2022-12-28 15:21:09 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XigniteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new getQuotes();
+            c.GetGCTQuotes();
+        }
+    }
+
+    public class getQuotes
+    {
+        xHist.XigniteGlobalHistorical api = new xHist.XigniteGlobalHistorical();
+
+        public void GetGCTQuotes()
+        {
+
+            xHist.GlobalHistoricalQuote[] call = api.GetGlobalHistoricalQuotes("GCT", "Symbol", null, "None", "12/27/2022");
+
+            for (int i = 0; i < call.Count(); i++)
+            {
+                var Outcome = call[i].Outcome;
+            }
+            
+        }
+           
+    }
+
+}
